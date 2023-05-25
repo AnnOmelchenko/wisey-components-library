a98d815c9829db3f2f8279c22e0759fa8b25e436
Anna Ostrovska <72460025+AnnaOstrovska@users.noreply.github.com>
2023-05-25 18:43:45 +0000

    update styles

--- dist/main.js
+++ dist/main.js
@@ -11,8 +11,7 @@ $parcel$export(module.exports, "Spinner", () => $3eb8b0f3caa86023$export$7f7cbe8
 const $3eb8b0f3caa86023$export$7f7cbe89f1eacd2 = ()=>/*#__PURE__*/ (0, $iA2ta$reactjsxruntime.jsx)("div", {
         className: "spinnerWrapper",
         children: /*#__PURE__*/ (0, $iA2ta$reactjsxruntime.jsx)("span", {
-            className: "spinner",
-            "data-testid": "spinner"
+            className: "spinner"
         })
     });
 
--- src/components/spinner/index.tsx
+++ src/components/spinner/index.tsx
@@ -1,7 +1,5 @@
 import React from "react"
 
-import './Spinner.styled.scss'
-
 export const Spinner = () => (
   <div className="spinnerWrapper">
     <span className="spinner" />
