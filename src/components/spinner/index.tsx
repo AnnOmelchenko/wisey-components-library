26857f0f26bf6eaf0ce5b8aa2fbc790e4d40f709
Anna Ostrovska <72460025+AnnaOstrovska@users.noreply.github.com>
2023-05-25 17:57:38 +0000

    0.0.2

---
+++ src/components/spinner/index.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+// import './Spinner.styled.scss'
+
+export const Spinner = () => (
+  <div className="spinnerWrapper">
+    <span className="spinner" data-testid="spinner" />
+  </div>
+)
